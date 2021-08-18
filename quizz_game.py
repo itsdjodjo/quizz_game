88ce3bb84d4b9fb09968ec661b4b0a246a4b30b3
captainJojo <tsoaramaherison7@gmail.com>
2021-08-18 15:36:30 +0200

    Added 10 questions

--- quizz_game.py
+++ quizz_game.py
@@ -18,28 +18,107 @@ else:
     print("Incorrect")
 
     time.sleep(2)  # time.sleep make my game more fun
-    print("The correct answer is : 'new delhi'")
+    print("The correct answer is : 'New Delhi'")
 
 
-answer = input("Who's the first President of USA ? ")
-if answer.lower() == "george washington":
+answer = input("What is the capital of Canada ? ")
+if answer.lower() == "toronto":
     score += 1
     print("Correct")
 else:
     print("Incorrect")
 
     time.sleep(2)
-    print("The correct answer is : 'george washington'")
+    print("The correct answer is : 'Toronto'")
 
-print("Who write one piece ? ", end=" ")
+
+print("What is the capital of USA ? ", end=" ")
 answer = input()
-if answer.lower() == "oda":
+if answer.lower() == "washington dc":
     score += 1
     print("Correct")
 else:
     print("Incorrect")
 
     time.sleep(2)
-    print("The correct answer is : 'Oda'")
+    print("The correct answer is : 'Washington DC'")
+
+
+answer = input("What is the capital of Brazil ? ")
+if answer.lower() == "brasilia":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Brasilia'")
+
+
+answer = input("What is the capital of Japan ? ")
+if answer.lower() == "tokyo":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Tokyo'")
+
+
+answer = input("What is the capital of Spain ?")
+if answer.lower() == "manchester":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Manchester'")
+
+
+answer = input("What is the capital of South Korea ? ")
+if answer.lower() == "seoul":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Seoul'")
+
+
+answer = input("What is the capital of China ? ")
+if answer.lower() == "beijing":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Beijing'")
+
+
+answer = input("What is the capital of Marroco ? ")
+if answer.lower() == "rabat":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Rabat'")
+
+
+answer = input("What is the capital of Senegal ? ")
+if answer.lower() == "dakar":
+    score += 1
+    print("Correct")
+else:
+    print("Incorrect")
+
+    time.sleep(2)
+    print("The correct answer is : 'Dakar'")
+
 
 print("You got " + str(score) + " question correct!")
